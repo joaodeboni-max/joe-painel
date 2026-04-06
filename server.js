8e12a94446204eadfed9d241fa012148ca665b13
joaodeboni-max <joaodeboni.rodapizza@gmail.com>
2026-04-06 12:44:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,10 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-
 const PORT = process.env.PORT || 3000;
-const DIST = path.join(__dirname, 'dist');
-
-const MIME = {
-  '.html': 'text/html',
-  '.js': 'application/javascript',
-  '.css': 'text/css',
-  '.json': 'application/json',
-  '.png': 'image/png',
-  '.svg': 'image/svg+xml',
-  '.ico': 'image/x-icon'
-};
+const HTML = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
 
 http.createServer((req, res) => {
-  let filePath = path.join(DIST, req.url === '/' ? 'index.html' : req.url);
-  if (!fs.existsSync(filePath)) filePath = path.join(DIST, 'index.html');
-  const ext = path.extname(filePath);
-  res.writeHead(200, { 'Content-Type': MIME[ext] || 'application/octet-stream' });
-  fs.createReadStream(filePath).pipe(res);
-}).listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end(HTML);
+}).listen(PORT, () => console.log('Server on port ' + PORT));
